b2e4ee616fdde058469a3fee6c1c22a8ef323509
Eric Lopes Pedroso <ericlopespedroso@gmail.com>
2026-09-05 15:36:33 -0300

    Criação de api gateway 2

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -38,13 +38,6 @@ resource "aws_apigatewayv2_route" "root_route" {
   target    = "integrations/${aws_apigatewayv2_integration.ecs_integration.id}"
 }
 
-# 4. Estágio Padrão ($default) com auto-deploy ativado
-resource "aws_apigatewayv2_stage" "default_stage" {
-  api_id      = aws_apigatewayv2_api.http_api.id
-  name        = "$default"
-  auto_deploy = true
-}
-
 # Output para exibir a URL final gerada pelo API Gateway
 output "api_gateway_url" {
   description = "URL HTTPS pública gerada pelo API Gateway (Free Tier)"
